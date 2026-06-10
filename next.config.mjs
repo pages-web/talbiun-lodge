239dcf7abf12f9e5e6c4b6817929f9e0594e1448
erxes <dev@erxes.io>
2026-06-10 21:19:21 +0800

    fix: add root redirect to /en

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,15 @@ const nextConfig = {
     NEXT_PUBLIC_ERXES_CMS_ID: "6a291a72aaeac693c821dd6c",
     ERXES_APP_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6ImFkZFZ4czUxUXlYdTlVSXNRc1FwRyIsImlhdCI6MTc4MTA3NzQyNX0.bvy5PqyIEorxfIIriXuoBjydHsN2GbtCjxsG_qvcbl4",
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
